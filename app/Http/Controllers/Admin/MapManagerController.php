4b2dedb54c954fba232c0b459846a26285514051
Andndre <agungandre687@gmail.com>
2026-05-30 15:54:54 +0800

    fix: polymorphic eager loading for quizzes in MapManagerController

--- app/Http/Controllers/Admin/MapManagerController.php
+++ app/Http/Controllers/Admin/MapManagerController.php
@@ -14,7 +14,12 @@ class MapManagerController extends Controller
      */
     public function index(): View
     {
-        $locations = MapLocation::with(['locationable', 'locationable.quizzes'])->get();
+        $locations = MapLocation::with(['locationable' => function (\Illuminate\Database\Eloquent\Relations\MorphTo $morphTo) {
+            $morphTo->morphWith([
+                \App\Models\CulturalObject::class => ['quizzes'],
+            ]);
+        }])->get();
+        
         $owners = User::where('role', 'umkm_owner')->orderBy('name')->get();
 
         return view('admin.map-manager.index', compact('locations', 'owners'));
