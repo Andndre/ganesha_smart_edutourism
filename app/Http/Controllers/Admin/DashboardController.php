79a060f17afce5336694a2be7e6872c045788277
Andndre <agungandre687@gmail.com>
2026-06-22 20:21:24 +0800

    fix: resolve localization issues for CapacityZone and TourPackage names and descriptions in controller caches

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -95,7 +95,12 @@ class DashboardController extends Controller
         // 5. Capacity Zones
         $locale = app()->getLocale();
         $zones = Cache::tags(['capacity'])->flexible("capacity_zones_active_array_{$locale}", [60, 300], function () use ($locale) {
-            return CapacityZone::where('is_active', true)->get()->append('occupancy_percentage')->toArray();
+            return CapacityZone::where('is_active', true)->get()->append('occupancy_percentage')
+                ->map(function ($zone) use ($locale) {
+                    $data = $zone->toArray();
+                    $data['name'] = \is_string($zone->name) ? $zone->name : ($zone->name[$locale] ?? $zone->name['en'] ?? '');
+                    return $data;
+                })->values()->toArray();
         });
         if (empty($zones)) {
             $zones = [
--- app/Http/Controllers/TourPackageController.php
+++ app/Http/Controllers/TourPackageController.php
@@ -11,7 +11,15 @@ class TourPackageController extends Controller
     {
         $locale = app()->getLocale();
         $packages = Cache::tags(['packages'])->flexible("tour_packages_active_array_{$locale}", [86400, 172800], function () use ($locale) {
-            return TourPackage::active()->get()->toArray();
+            return TourPackage::active()->get()->map(function ($package) use ($locale) {
+                $data = $package->toArray();
+                foreach (['name', 'description'] as $field) {
+                    if (isset($data[$field]) && is_array($data[$field])) {
+                        $data[$field] = $data[$field][$locale] ?? $data[$field][config('app.fallback_locale')] ?? reset($data[$field]) ?? '';
+                    }
+                }
+                return $data;
+            })->values()->toArray();
         });
 
         return view('user.packages.index', compact('packages'));
